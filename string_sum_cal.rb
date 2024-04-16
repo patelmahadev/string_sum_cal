8c42fc6604f55268aef8f8d1b285448a91eec0fe
mahadev <mahadev.patel1227@gmail.com>
2024-04-16 11:12:47 +0530

    added test case for new line

--- string_sum_cal.rb
+++ string_sum_cal.rb
@@ -3,28 +3,31 @@ require 'minitest/autorun'
 class StringSumCal
   def self.add(values)
     return 0 if values.empty?
-    values = values.split(/,|\n/)
-    values.map(&:to_i).sum
+    values.split(/,|\n/).map(&:to_i).sum
   end
 end
 # 
 puts StringSumCal.add("") # => 0
 puts StringSumCal.add("2") # => 2
 puts StringSumCal.add("3,5") # => 8
-
+puts StringSumCal.add("6\n4,8") # => 18
 #  test Cases
 class StringSumCalTest < Minitest::Test
   # Test for adding with an empty string value.
   def test_addition_with_empty_string
     assert_equal 0, StringSumCal.add("")
   end
-  # Test for adding with a number value
+  # Test for adding with a number value.
   def test_addition_with_one_number
     assert_equal 2, StringSumCal.add("2")
   end
   # Test for adding with a Two number values
   def test_addition_with_two_numbers
-    assert_equal 8, StringSumCal.add("6,2")
+    assert_equal 15, StringSumCal.add("6,9")
+  end
+  # Test for adding with new lines between numbers.
+  def test_add_with_new_lines_between_numbers
+    assert_equal 14, StringSumCal.add("2\n3,9")
   end
 
 end
